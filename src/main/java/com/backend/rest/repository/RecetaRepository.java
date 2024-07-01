0d4036e8cae280032b872529534a66d6b327207b
Sebastián Giménez Garrido Lecca <sebastggl@gmail.com>
2024-07-01 01:24:48 -0500

    Receta añadida.

--- src/main/java/com/backend/rest/repository/RecetaRepository.java
+++ src/main/java/com/backend/rest/repository/RecetaRepository.java
@@ -1,9 +1,15 @@
 package com.backend.rest.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.backend.rest.entity.Receta;
 
 public interface RecetaRepository extends JpaRepository<Receta, Integer>{
-
+	@Query("select r from Receta r where r.medico=?1")
+	public List<Receta> findAllRecetaByMedico(Integer cod);
+	@Query("select r from Receta r where r.paciente=?1")
+	public List<Receta> findAllRecetaByPaciente(Integer cod);
 }
